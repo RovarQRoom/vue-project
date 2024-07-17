332fbb5d66bff02ac7a24eddb6b906d37ffe710d
RovarQRoom <rovar.07000120@outlook.com>
2024-07-17 14:11:55 +0300

    chore: Remove unnecessary files and code

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,8 +6,9 @@ module.exports = {
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
+    'plugin:vue/vue3-recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
   ],
   parserOptions: {
     ecmaVersion: 'latest'
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('tailwindcss-children'),
+  ],
+}
+
